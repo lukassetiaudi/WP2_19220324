9caaf601be907a349b0e9a4af589f61a53e89f6a
lukassetiaudi <lukassetiaudi@gmail.com>
2023-11-27 23:36:13 +0700

    Pembuatan Halaman ubah profil dan penambahan kategory buku

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,24 +2,24 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 class Admin extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->helper('Pustaka');
-        $this->load->library('session');
-        $this->load->database(); // Memuat library database
-        cek_login();
-    }
-    public function index()
-    {
-        $data['judul'] = 'Dashboard';
-        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
-        $data['anggota'] = $this->ModelUser->getUserLimit()->result_array();
-        $data['buku'] = $this->ModelBuku->getBuku()->result_array();
-        $this->load->view('admin/header', $data);
-        $this->load->view('admin/sidebar', $data);
-        $this->load->view('admin/topbar', $data);
-        $this->load->view('admin/index', $data);
-        $this->load->view('admin/footer');
-    }
+
+public function __construct()
+{
+parent::__construct();
+cek_login();
+}
+
+public function index()
+{
+    $data['judul'] = 'Dashboard';
+    $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+    $data['anggota'] = $this->ModelUser->getUserLimit()->result_array();
+    $data['buku'] = $this->ModelBuku->getBuku()->result_array();
+    $this->load->view('templates/header', $data);
+    $this->load->view('templates/sidebar', $data);
+    $this->load->view('templates/topbar', $data);
+    $this->load->view('admin/index', $data);
+    $this->load->view('templates/footer');
+}
+
 }
